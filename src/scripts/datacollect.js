0a1183104801265f1c83442fc55a6cbea3e4b7b2
Shaddamah <shaddam.a.h@gmail.com>
2020-05-26 20:27:01 +0700

    Fix API Fetch

--- src/scripts/datacollect.js
+++ src/scripts/datacollect.js
@@ -1,13 +1,7 @@
 class DataCollect{
     static province() {
         return fetch(`https://api.kawalcorona.com/indonesia/provinsi/`)
-            .then(response => {
-                return response.json();
-            })
-            .then(dataResponse => {
-                // console.log(dataResponse);
-                return Promise.resolve(dataResponse);
-            })
+            .then(response => response.json())
             .catch(error => {
                 return Promise.reject(error);
         })
@@ -32,31 +26,14 @@ class DataCollect{
             fetch(`https://api.kawalcorona.com/meninggal/`)
         ])
         .then(responses => {
-            return responses.map(response => {
-                return response.json();
-            });
+            return responses.map(response => response.json());
         })
-        .then(datasResponse => {
-            const globalData = [{
-                "name": "di dunia",
-                "positif": null,
-                "sembuh": null,
-                "meninggal": null
-            }];
-            return datasResponse[0].then(v => {
-                globalData[0].positif = v.value;
-                return datasResponse[1].then(v => {
-                    globalData[0].sembuh = v.value;
-                    return datasResponse[2].then(v => {
-                        globalData[0].meninggal = v.value;
-                        return Promise.resolve(globalData);
-                    })
-                })
-            })
-        })
-        .catch(error => {
-            return Promise.reject(error);
-        });
+        .then(async (datasResponse) => [{
+            "name": "di dunia",
+            "positif": (await datasResponse[0]).value,
+            "sembuh": (await datasResponse[1]).value,
+            "meninggal": (await datasResponse[2]).value
+        }]);
     }
 }
 export default DataCollect;
